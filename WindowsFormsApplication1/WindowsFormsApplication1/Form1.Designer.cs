4271f3597190bea5039fb959af851c1f30a1ec8f
istemi <istemidulger@gmail.com>
2013-07-27 19:34:19 +0200

    init commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.list = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(376, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "List Purge";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // list
+            // 
+            this.list.AllowColumnReorder = true;
+            this.list.AutoArrange = false;
+            this.list.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.list.Location = new System.Drawing.Point(12, 12);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(323, 327);
+            this.list.Sorting = System.Windows.Forms.SortOrder.Descending;
+            this.list.TabIndex = 1;
+            this.list.UseCompatibleStateImageBehavior = false;
+            this.list.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Months AFK";
+            this.columnHeader1.Width = 109;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            this.columnHeader2.Width = 495;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(373, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 133);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Waiting Input...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(376, 113);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(145, 22);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(373, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Minimum Months Afk";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(373, 287);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 50);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Print Purge List To A Text File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 362);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.list);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "PurgeWatch";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView list;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        List<bro> warbrostopurge = new List<bro>();
+        int purgedate;
+        int curbrocount;
+        public Form1()
+        {
+            InitializeComponent();
+            purgedate = 1;
+        }
+
+        public void Init()
+        {
+            warbrostopurge.Clear();
+            string[] lines = System.IO.File.ReadAllLines(@"Warbros.csv");
+            curbrocount = lines.Count();
+            char[] chrs;
+            char[] chrs2;
+            //char[] dchrs = new char[3];
+            char[] mchrs = new char[3];
+            //char[] nchrs = new char[2];
+            for (int i = 0; i < curbrocount; i++)
+            {
+                bro curbro = new bro();
+                int a = lines[i].Count();
+                chrs = new char[a];
+                chrs2 = new char[a];
+                lines[i].Count();
+
+                int b = lines[i].IndexOf(',') + 1;
+
+                chrs = new char[b];
+                chrs2 = new char[a-b];
+                lines[i].CopyTo(0, chrs, 0, b);
+                lines[i].CopyTo(b, chrs2, 0, a - b);
+
+                curbro.date = new string(chrs);
+                curbro.name = new string(chrs2);
+
+                curbro.date.CopyTo(4, mchrs, 0, 3);
+                //curbro.date.CopyTo(7, nchrs, 0, 2);
+
+                string m = new string(mchrs);
+                //string d = new string(nchrs);
+
+                switch (m)
+                {
+                    case ("Jan"):
+                        curbro.months = 1;
+                        break;
+                    case ("Feb"):
+                        curbro.months = 2;
+                        break;
+                    case ("Mar"):
+                        curbro.months = 3;
+                        break;
+                    case ("Apr"):
+                        curbro.months = 4;
+                        break;
+                    case ("May"):
+                        curbro.months = 5;
+                        break;
+                    case ("Jun"):
+                        curbro.months = 6;
+                        break;
+                    case ("Jul"):
+                        curbro.months = 7;
+                        break;
+                    case ("Aug"):
+                        curbro.months = 8;
+                        break;
+                    case ("Sep"):
+                        curbro.months = 9;
+                        break;
+                    case ("Oct"):
+                        curbro.months = 10;
+                        break;
+                    case ("Nov"):
+                        curbro.months = 11;
+                        break;
+                    case ("Dec"):
+                        curbro.months = 12;
+                        break;
+                    default:
+                        break;
+                }
+
+                if ((DateTime.Today.Month - curbro.months) >= purgedate)
+                {
+                    list.Items.Add((DateTime.Today.Month - curbro.months).ToString()).SubItems.Add(curbro.name);
+                    warbrostopurge.Add(curbro);
+                }
+
+                label1.Text = "Processed " + curbrocount.ToString() + " Members And Found " + list.Items.Count.ToString() + " Of Them Suitable For Purge.";
+            }
+            button2.Enabled = true;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            list.Items.Clear();
+            list.Columns.Add("Months AFK");
+            list.Columns.Add("Name");
+            Init();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<string> print = new List<string>();
+            print.Add("Purge List With Members Afk For " + purgedate.ToString() + " Or More Months.");
+            print.Add(warbrostopurge.Count.ToString() + " Members Listed Out Total " + curbrocount.ToString() + " Are Found Wanting.\r\n");
+            print.Add("Months AFK - Name\r\n");
+
+
+            warbrostopurge.Sort((x, y) => x.months.CompareTo(y.months));
+
+            for (int i = 0; i < warbrostopurge.Count(); i++)
+			{
+                print.Add((DateTime.Today.Month - warbrostopurge[i].months)+ " - " + warbrostopurge[i].name);
+			}
+            print.Add("\r\n\r\nGenerated Using Purge Watch By KONAir");
+            print.Add("\r\n\r\nEnd Of File - Purge The AFK - ");
+            System.IO.File.WriteAllLines(@"WarbrosPurgeList.txt", print);
+
+            System.Diagnostics.Process.Start(@"WarbrosPurgeList.txt");
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            purgedate = Convert.ToInt32(numericUpDown1.Value);
+        }
+
+
+    }
+}
